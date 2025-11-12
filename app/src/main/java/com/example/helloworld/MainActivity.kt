dea66562aaa0f8e99e2e7483f9f2b4de1775c96c
FerSV4 <ferandressc@hotmail.com>
2025-11-12 14:11:52 -0400

    Tarea permisos de android

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.helloworld
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -13,18 +14,17 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.helloworld.ui.theme.HelloWorldTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             HelloWorldTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    EjemploRow(modifier = Modifier.padding(innerPadding))
+                Scaffold(modifier = Modifier.fillMaxSize()) { padding ->
+                    PermisosView(modifier = Modifier.padding(padding))
                 }
             }
         }
@@ -32,25 +32,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun EjemploRow(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.fillMaxSize()) {
-        Text(
-            text = "Hola APP",
-            modifier = Modifier.weight(1f)
-        )
+fun PermisosView(modifier: Modifier = Modifier) {
+    val pedirMicrofono = rememberLauncherForActivityResult(RequestPermission()) {}
+    val pedirUbicacion = rememberLauncherForActivityResult(RequestPermission()) {}
+
+    Column(modifier = modifier.fillMaxSize().padding(24.dp)) {
         Button(
-            onClick = {},
-            modifier = Modifier.weight(1f)
+            onClick = { pedirMicrofono.launch(Manifest.permission.RECORD_AUDIO) },
+            modifier = Modifier.padding(bottom = 16.dp)
         ) {
-            Text("UCB")
+            Text("Pedir micrófono")
+        }
+        Button(
+            onClick = { pedirUbicacion.launch(Manifest.permission.ACCESS_FINE_LOCATION) },
+            modifier = Modifier.padding(bottom = 16.dp)
+        ) {
+            Text("Pedir ubicación")
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun EjemploRowPreview() {
+fun PermisosViewPreview() {
     HelloWorldTheme {
-        EjemploRow()
+        PermisosView()
     }
 }
